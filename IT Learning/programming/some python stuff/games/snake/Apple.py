6a6026a7da0531a70401187e6246151ded1f41a2
MedrekIT <dan.creez@o2.pl>
2024-11-01 00:03:03 +0100

    Explained snake projects in comments

--- IT Learning/programming/some python stuff/games/snake/Apple.py
+++ IT Learning/programming/some python stuff/games/snake/Apple.py
@@ -3,6 +3,7 @@ import random as rd
 
 class Apple:
     def __init__(self, snake):
+        #Initializing apple coordinates
         self.applePosX: int = rd.randrange(0, 75, 5)
         self.applePosY: int = rd.randrange(0, 75, 5)
 
@@ -10,5 +11,6 @@ class Apple:
             self.applePosX = rd.randrange(0, 75, 5)
             self.applePosY = rd.randrange(0, 75, 5)
 
+    #Drawing an apple
     def drawapple(self, WIN):
         pg.draw.rect(WIN, (255, 75, 0), (self.applePosX * 10, self.applePosY * 10, 50, 50), 0, 3)
--- IT Learning/programming/some python stuff/games/snake/GameLogic.py
+++ IT Learning/programming/some python stuff/games/snake/GameLogic.py
@@ -3,6 +3,7 @@ from time import time
 
 def checkcollision(tickSpeed, slowness, snake, apple, direction, previous):
     headx, heady = snake.cords[0]
+    #Checking if snake's head is touching its body
     if (direction == 'w' and snake.cords[1] in {(headx, heady - 5), (headx, heady + 75)} or
             direction == 's' and snake.cords[1] in {(headx, heady + 5), (headx, heady - 75)} or
             direction == 'a' and snake.cords[1] in {(headx - 5, heady), (headx + 75, heady)} or
@@ -14,6 +15,7 @@ def checkcollision(tickSpeed, slowness, snake, apple, direction, previous):
                 direction == 'a' and ((i[0] + 5, i[1]) == snake.cords[0] or (i[0] - 75, i[1]) == snake.cords[0]) or
                 direction == 'd' and ((i[0] - 5, i[1]) == snake.cords[0] or (i[0] + 75, i[1]) == snake.cords[0])):
             return False
+    #Checking if snake's head is going to eat an apple
     if (direction == 'w' and (
             (apple.applePosX, apple.applePosY + 5) == snake.cords[0] or (apple.applePosX, apple.applePosY - 75) ==
             snake.cords[0]) or
@@ -25,18 +27,24 @@ def checkcollision(tickSpeed, slowness, snake, apple, direction, previous):
                     apple.applePosX + 75, apple.applePosY) == snake.cords[0])):
         snake.cords.insert(0, (apple.applePosX, apple.applePosY))
         snake.snakeSize += 1
+
+        #Speeding up the game when an apple is eaten
         if slowness > 4:
             slowness -= 5 if slowness % 2 == 0 else 1
         else:
             tickSpeed += 5 if tickSpeed % 2 == 0 else 1
+        #Generating new apple
         apple.__init__(snake)
     return True
 
 def drawgame(WIN, gameFont, gameTime, snake, apple):
+    #Drawing objects
     snake.drawsnake(WIN)
     apple.drawapple(WIN)
 
+    #Drawing scoreboard
     scoreboard = gameFont.render(f'{int(time()-gameTime)} SCORE = {snake.snakeSize - 2}', True, (255, 255, 255), None)
     WIN.blit(scoreboard, (50, 50))
 
+    # Refreshing game screen
     pg.display.update()
